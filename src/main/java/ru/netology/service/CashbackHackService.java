60e0fe04ba0ff0d5fbbe1fbf484cf05683ab5b5a
Anton <anton.za90@gmail.com>
2026-03-03 18:09:15 +0300

    active bug

--- src/main/java/ru/netology/service/CashbackHackService.java
+++ src/main/java/ru/netology/service/CashbackHackService.java
@@ -4,8 +4,6 @@ public class CashbackHackService {
     private final int boundary = 1000;
 
     public int remain(int amount) {
-        if (boundary > amount) {
-            return (boundary - amount) % boundary;
-        } else return (amount - boundary) % boundary;
+        return boundary - amount % boundary;
     }
 }
